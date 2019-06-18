7057e25630400c9d3b3d94c5c57dc77212288f4b
Drew Gillson <drew.gillson@looker.com>
2019-06-18 09:08:31 -0600

    Updates

---
+++ precache-manifest.1180647b7bbc31272f1b5e99f664dc40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e6fdc054e69719860a1d3ab68501993",
+    "url": "/transcriber/index.html"
+  },
+  {
+    "revision": "af79d7340346a056cdc2",
+    "url": "/transcriber/static/css/2.fb51fcd7.chunk.css"
+  },
+  {
+    "revision": "40293002462882f68c18",
+    "url": "/transcriber/static/css/main.912fc368.chunk.css"
+  },
+  {
+    "revision": "af79d7340346a056cdc2",
+    "url": "/transcriber/static/js/2.8b255b58.chunk.js"
+  },
+  {
+    "revision": "40293002462882f68c18",
+    "url": "/transcriber/static/js/main.a40653c8.chunk.js"
+  },
+  {
+    "revision": "2fac21ad8ab7cbfd5d50",
+    "url": "/transcriber/static/js/runtime~main.90b87191.js"
+  },
+  {
+    "revision": "c35b2089d33eb7a6da51834b007a69a3",
+    "url": "/transcriber/static/media/logo.c35b2089.svg"
+  }
+]);
